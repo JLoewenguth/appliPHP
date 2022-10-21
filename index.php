25f1da89ca5b2297fff0683eeb8392af19bc9149
JLoewenguth <jrm.lguth@gmail.com>
2022-10-21 16:45:17 +0200

    partie 2 - messages de traitement

--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 
 <!DOCTYPE html>
 <html lang="en">
+<div id="bg">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<!--CSS-->
+        <link rel="stylesheet" href="style.css">
         <title>Ajout produit</title>
     </head>
 
     <body>
 
-        <h1>Ajouter un produit</h1>
+    <div id="encadre"><h2>Ajouter un produit :</h2>
         <form action="traitement.php" method="post">
             <p>
                 <label>
@@ -32,15 +34,21 @@
                 </label>
             </p>
             <p>
-                <input type="submit" name="submit" value="Ajouter le produit">
+                <div class="myButton"><input type="submit" name="submit" value="Ajouter le produit"></div>
             </p>
-        </form>
-    </body>
-
+        </form></div>
+    </body></div>
+<!-- lien vers le récapitulatif produits -->
+</br><a href="recap.php">Récapitulatif des produits</a>
 </html>
 
 <?php
+/*echo compteProducts();*/
 
+//message de traitement.php
+if(isset($_GET['Message'])){
+    echo $_GET['Message'];
+}
 
 
 ?>
--- traitement.php
+++ traitement.php
@@ -2,7 +2,7 @@
     session_start();
 
     if(isset($_POST['submit'])){        //recherche la clé 'submit'
-        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
         $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
         $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
 
@@ -14,11 +14,14 @@
                 "qtt" => $qtt,
                 "total" => $price*$qtt
             ];
-
-            $_SESSION['products'][] = $product;         //enregistrement produit
+            $Message = urlencode("</br></br>Liste mise à jour.");       //message en cas de nouvelles données
+            $_SESSION['products'][] = $product;                         //enregistrement produit
+        }   else{
+            $Message = urlencode("</br></br>Données non valides.");     //message en cas de données invalides
         }
     }
 
-    header("Location:index.php");       //retour à l'index
+    header("Location:index.php?Message=".$Message);
+    die;       //retour à l'index
 
 ?>
